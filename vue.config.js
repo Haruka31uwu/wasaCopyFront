6c87ddd15a196e0d0d8aa720952b053cd0e5d35d
Haruka <francis.torres@alarcongroup.us>
2023-08-09 22:46:34 -0500

    .

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:"/wasaCopyFront/"
+  publicPath: process.env.NODE_ENV === 'production'?'/wasaCopyFront/':'/',
+
 })
